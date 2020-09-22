490d289d6e90a22d9708c210e75b5a8c0e868500
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 10:35:08 +0530

    7:@Autowiring using byName. 6 was @Autowiring using byType

--- src/com/ansul/thakur/Human.java
+++ src/com/ansul/thakur/Human.java
@@ -1,6 +1,7 @@
 package com.ansul.thakur;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Human {
 	
