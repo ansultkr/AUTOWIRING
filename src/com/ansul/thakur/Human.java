34f50952659dd26f2d444e240a4fde02017d1bc0
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 09:54:34 +0530

    3: Autowiring using Constructors. Make sure you have constructor

--- src/com/ansul/thakur/Human.java
+++ src/com/ansul/thakur/Human.java
@@ -1,7 +1,12 @@
 package com.ansul.thakur;
 
 public class Human {
+	
 	private Heart heart;
+
+	public Human(Heart heart) {
+		this.heart = heart;
+	}
 
 	public void setHeart(Heart heart) {
 		this.heart = heart;
