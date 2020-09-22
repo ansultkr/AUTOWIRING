6330afa7d54b815695902926f4c450ddefa9ff7e
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 10:23:51 +0530

    6: @Autowired using setter

--- src/com/ansul/thakur/Human.java
+++ src/com/ansul/thakur/Human.java
@@ -10,12 +10,12 @@ public class Human {
 		
 	}
 	
-	@Autowired
+	
 	public Human(Heart heart) {
 		this.heart = heart;
 		System.out.println("Human Constructor is getting called whicha has Heart as argument");
 	}
-
+	@Autowired
 	public void setHeart(Heart heart) {
 		this.heart = heart;
 		System.out.println("Setter Mathod Called! @Autowiring byName");
