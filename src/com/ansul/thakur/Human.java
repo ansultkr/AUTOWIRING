df9c78678d6fa659eda8c6e9fb1e1f8ce401cb3a
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 10:20:46 +0530

    5: Autowiring using @Annotations but make sure u add  <context:annotation-config/> in your file

--- src/com/ansul/thakur/Human.java
+++ src/com/ansul/thakur/Human.java
@@ -1,11 +1,19 @@
 package com.ansul.thakur;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Human {
 	
 	private Heart heart;
-
+	
+	public Human() {
+		
+	}
+	
+	@Autowired
 	public Human(Heart heart) {
 		this.heart = heart;
+		System.out.println("Human Constructor is getting called whicha has Heart as argument");
 	}
 
 	public void setHeart(Heart heart) {
