301d3d9c2c8b06cdf04cd91106073a335359ea3f
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-22 10:49:47 +0530

    9: littel modified example of 8

--- src/com/ansul/thakur/Heart.java
+++ src/com/ansul/thakur/Heart.java
@@ -1,8 +1,28 @@
 package com.ansul.thakur;
 
 public class Heart {
+	
+	private String nameOfAnimal;
+	private int noOfHeart;
+	
 	public void pump() {
 		System.out.println("Your Heart is pumping!");
+	}
+
+	public String getNameOfAnimal() {
+		return nameOfAnimal;
+	}
+
+	public void setNameOfAnimal(String nameOfAnimal) {
+		this.nameOfAnimal = nameOfAnimal;
+	}
+
+	public int getNoOfHeart() {
+		return noOfHeart;
+	}
+
+	public void setNoOfHeart(int noOfHeart) {
+		this.noOfHeart = noOfHeart;
 	}
 
 }
--- src/com/ansul/thakur/Human.java
+++ src/com/ansul/thakur/Human.java
@@ -17,15 +17,18 @@ public class Human {
 		System.out.println("Human Constructor is getting called whicha has Heart as argument");
 	}
 	@Autowired
-	@Qualifier("octopousHeart")
+	@Qualifier("humanHeart")
 	public void setHeart(Heart heart) {
 		this.heart = heart;
 		System.out.println("Setter Mathod Called! @Autowiring byName");
+		
 	}
 	
 	public void startPumping() {
 		if(heart!=null) {
 		heart.pump();
+		System.out.println(heart.getNameOfAnimal()+" ::Name of the Animal");
+		System.out.println(heart.getNoOfHeart()+" ::Number of Heart in the Animal");
 		}
 		else System.out.println("Your dead");
 	}
